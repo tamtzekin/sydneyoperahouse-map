4a28f1cd1bab0f0d89fdf73c7553d9b67d1c5528
Justin Tam <tamtzekin@gmail.com>
2020-05-28 12:44:59 +1000

    Upload code

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/* Display venue names on hover over venue */
+$('.clc-highlight').hover(() => {
+  $('.clc-name').toggleClass('showed');
+});
+
+$('.concerthall-highlight').hover(() => {
+  $('.concerthall-name').toggleClass('showed');
+});
+
+$('.entryfoyers-highlight').hover(() => {
+  $('.entryfoyers-name').toggleClass('showed');
+});
+
+$('.jst-highlight').hover(() => {
+    $('.jst-name').toggleClass('showed');
+});
+
+$('.yallamundi-highlight').hover(() => {
+  $('.yallamundi-name').toggleClass('showed');
+});
